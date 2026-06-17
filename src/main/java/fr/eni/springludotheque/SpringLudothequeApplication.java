d89055d2eb8923976e55f24f95bbef0d5b7a6a92
julb29 <leblay.julien2026@campus-eni.fr>
2026-06-17 12:12:32 +0200

    version initiale

---
+++ src/main/java/fr/eni/springludotheque/SpringLudothequeApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.springludotheque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLudothequeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLudothequeApplication.class, args);
+    }
+
+}
