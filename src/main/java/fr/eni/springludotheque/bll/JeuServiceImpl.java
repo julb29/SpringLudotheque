000466d1817d2a2fbdd3683c43d916f9c9f3d3b9
julb29 <leblay.julien2026@campus-eni.fr>
2026-06-19 12:00:50 +0200

    S2012 - Couche BLL : ajout d'un jeu (et des liens vers les genres) + Tests

---
+++ src/main/java/fr/eni/springludotheque/bll/JeuServiceImpl.java
@@ -0,0 +1,23 @@
+package fr.eni.springludotheque.bll;
+
+import fr.eni.springludotheque.bo.Jeu;
+import fr.eni.springludotheque.dal.JeuRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JeuServiceImpl implements JeuService {
+
+    private final JeuRepository jeuRepository;
+
+    @Autowired
+    public JeuServiceImpl(JeuRepository jeuRepository) {
+        this.jeuRepository = jeuRepository;
+    }
+
+    @Override
+    public void creationJeu(Jeu jeu) {
+        jeuRepository.save(jeu);
+    }
+
+}
---
+++ src/test/java/fr/eni/springludotheque/bll/JeuServiceimplTest.java
@@ -0,0 +1,49 @@
+package fr.eni.springludotheque.bll;
+
+import fr.eni.springludotheque.bo.Genre;
+import fr.eni.springludotheque.bo.Jeu;
+import fr.eni.springludotheque.dal.JeuRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class JeuServiceimplTest {
+
+    @Autowired
+    private JeuService jeuService;
+    @Autowired
+    private JeuRepository jeuRepository;
+
+    @Test
+    public void creationJeu() {
+
+        // Arrange
+        Jeu jeuTest = new Jeu("3456789012345", "Tomb raider", 20d);
+        Genre genreTest = new Genre(1);
+
+        jeuTest.setGenres(List.of(genreTest));
+
+        // Act
+        jeuService.creationJeu(jeuTest);
+
+        // Assert (Correction du commentaire au passage !)
+        Jeu jeuBD = jeuRepository.findById(jeuTest.getId()).orElse(null);
+
+        // 1. On vérifie que le jeu existe bien en BDD
+        Assertions.assertNotNull(jeuBD);
+
+        // 2. On compare les attributs un par un
+        Assertions.assertEquals(jeuTest.getTitre(), jeuBD.getTitre());
+        Assertions.assertEquals(jeuTest.getTarif(), jeuBD.getTarif());
+
+        // 3. On vérifie les genres associés
+        Assertions.assertNotNull(jeuBD.getGenres());
+        Assertions.assertEquals(1, jeuBD.getGenres().size()); // On vérifie qu'il y a bien 1 genre
+        Assertions.assertEquals(1, jeuBD.getGenres().get(0).getId()); // On vérifie que c'est bien l'ID 1
+    }
+
+}
